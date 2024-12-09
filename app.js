996af1f26140bd2145125cd927ef16668e0989cc
Hanvitha-1464 <petetihanvitha.231it048@nitk.edu.in>
2024-12-09 19:01:46 +0530

    Calculator

---
+++ app.js
@@ -0,0 +1,54 @@
+function computeadd(){
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById('output').innerText = 'Please Enter Valid Numbers';
+        return;
+    }
+
+    const result = num1 + num2;
+    document.getElementById('output').innerText = result;
+
+}
+
+function computesub(){
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById('output').innerText = 'Please Enter Valid Numbers';
+        return;
+    }
+
+    const result = num1 - num2;
+    document.getElementById('output').innerText = result;
+
+}
+function computemul(){
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById('output').innerText = 'Please Enter Valid Numbers';
+        return;
+    }
+
+    const result = num1 * num2;
+    document.getElementById('output').innerText = result;
+
+}
+
+function computediv(){
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById('output').innerText = 'Please Enter Valid Numbers';
+        return;
+    }
+
+    const result = num1 / num2;
+    document.getElementById('output').innerText = result;
+
+}
